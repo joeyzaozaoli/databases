288fe7f6c57cd61c49f2ceabd4d738df781014ba
Joey Li <joeyzaozaoli@gmail.com>
2017-08-20 20:58:34 -0700

    Design schema for chat messages

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,31 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username CHAR(35) NOT NULL,
+
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomname CHAR(35) NOT NULL,
 
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  text TEXT(135) NOT NULL,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  createdAt TIMESTAMP,
 
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
