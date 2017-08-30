bdb4f49c6d78ea87c082ba33fc34a81918a29a93
Joey Li <joeyzaozaoli@gmail.com>
2017-08-29 20:55:51 -0700

    Refactor request handler using Express

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,48 +1,32 @@
 var models = require('../models');
 
-var headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-headers': 'content-type, accept'
-};
-
 module.exports = {
   messages: {
     get: function (req, res) {
       models.messages.get(function(data) {
-        headers['Content-Type'] = 'application/json';
-        res.writeHead(200, headers);
-        res.end(JSON.stringify({results: data}));
+        res.json({results: data});
       });
     },
 
     post: function (req, res) {
       var msgObj = req.body;
       models.messages.post(msgObj.username, msgObj.roomname, msgObj.text, function() {
-        res.writeHead(201, headers);
-        res.end(JSON.stringify(null));
+        res.sendStatus(201);
       });
-    },
-
-    options: function(req, res) {
-      res.writeHead(200, headers);
-      res.end();
     }
   },
 
   users: {
     get: function (req, res) {
       models.users.get(function(data) {
-        headers['Content-Type'] = 'application/json';
-        res.writeHead(200, headers);
-        res.end(JSON.stringify({users: data}));
+        res.json({results: data});
       });
     },
 
     post: function (req, res) {
       var userObj = req.body;
       models.users.post(userObj.username, function() {
-        res.writeHead(201, headers);
-        res.end(JSON.stringify(null));
+        res.sendStatus(201);
       });
     }
   }
